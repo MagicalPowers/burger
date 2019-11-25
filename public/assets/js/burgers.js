2f2092c1379b17e82f043ab4b2d231d875931247
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-11-25 06:40:55 -0500

    updated burger.js, updated server.js, updated burger-block.handlebars

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,38 @@
 //not quite sure what needs to be here. 
+
+$(function () {
+    $(".switch-devoured").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("newdevoured");
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function () {
+                console.log("changed devoured to", newDevoured);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+        var newBurger = {
+            name: $("#burger").val().trim(),
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new catburglar");
+                location.reload();
+            }
+        );
+    });
+});
